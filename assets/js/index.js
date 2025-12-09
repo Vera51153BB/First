e1203406d7f2c27f6c327e6a1980304925483722
Vera51153BB <warmspringge@gmail.com>
2025-12-10 00:02:28 +0300

    Добавил пару токенов

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,11 @@ const TICKER_HOME_CONFIG = {
     { id: "litecoin", label: "LTC·USDT" },
     { id: "starcoin", label: "STRC·USDT" },
     { id: "shiba-inu", label: "SHIB·USDT" },
-    { id: "okb", label: "OKB·USDT" }
+    { id: "okb", label: "OKB·USDT" },
+    { id: "vechain", label: "VeChain·USDT" }
+    { id: "pump-fun", label: "PUMP·USDT" }
+    { id: "og-fan-token", label: "OG·USDT" }
+    
   ],
 
   // Параметры обновления

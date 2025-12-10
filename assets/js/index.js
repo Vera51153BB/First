c9413f89382c18f806d06042caf96ec294934368
Vera51153BB <warmspringge@gmail.com>
2025-12-10 11:46:09 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 // Конфиг главного тикера (_home_)
 const TICKER_HOME_CONFIG = {
-  // Базовые размеры исходной картинки
-  imgWidth: 1344,
-  imgHeight: 768,
+  // Масштабирование под ширину окна
+  get width() {
+    return window.innerWidth || document.documentElement.clientWidth;
+  },
+  get height() {
+    return Math.max(120, Math.min(window.innerHeight * 0.1, 180)); // гибкая высота
+  },
 
   // Список монет с CoinGecko для отображения (можно менять)
   symbols: [

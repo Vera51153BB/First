43e0faa0b06523c3c2c15d94a171ac230ea54bd4
Vera51153BB <warmspringge@gmail.com>
2026-01-28 23:04:58 +0300

    обновил передачу языка

--- assets/js/alerts.page.js
+++ assets/js/alerts.page.js
@@ -72,14 +72,16 @@ function render(){
       <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33h.09a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51h.09a1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82v.09a1.65 1.65 0 0 0 1.51 1H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
     </g>
   </svg>`;
+
     gearBtn.addEventListener('click', (e)=>{
       e.stopPropagation();
+      const qs = window.location.search || ""; // сохраняем ?lang=...
       if (a.id === 'alert_rsi') {
         // Настройки RSI
-        window.location.href = 'setting_alerts_rsi.html';
+        window.location.href = 'setting_alerts_rsi.html' + qs;
       } else if (a.id === 'alert_ema') {
-        // Новая страница настроек EMA
-        window.location.href = 'setting_alerts_ma.html';
+        // Настройки EMA
+        window.location.href = 'setting_alerts_ma.html' + qs;
       } else {
         // Для будущих уведомлений 3–4 показываем "Скоро"
         window.Core.showToast(tCommon('coming_soon'));
